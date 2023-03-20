279aaea5f7aaf7fc3c6f2f353df00f9f55e4890b
Innocent-Mbanda <59844750+Innocent-Mbanda@users.noreply.github.com>
2023-03-20 00:06:35 -0400

    my first commit

---
+++ src/main/java/Newsroom/shecancode_tekcode/ShecancodeTekcodeApplication.java
@@ -0,0 +1,13 @@
+package Newsroom.shecancode_tekcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShecancodeTekcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShecancodeTekcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Newsroom/shecancode_tekcode/ShecancodeTekcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package Newsroom.shecancode_tekcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShecancodeTekcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
